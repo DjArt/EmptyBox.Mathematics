ef0b61ac3f0765cbed5ecb2d08a9016c1be14c93
DjArt <art.letter.mail@gmail.com>
2017-11-30 17:53:35 +0800

    Version change

--- EmptyBox.Mathematics/AssemblyInfo.fs
+++ EmptyBox.Mathematics/AssemblyInfo.fs
@@ -28,8 +28,8 @@ open System.Runtime.InteropServices
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [<сборка: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("0.0.2.0")>]
-[<assembly: AssemblyFileVersion("0.0.2.0")>]
+[<assembly: AssemblyVersion("0.0.3.0")>]
+[<assembly: AssemblyFileVersion("0.0.3.0")>]
 
 do
     ()
